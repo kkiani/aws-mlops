94b647490275231a401a4f13e5b73dfcb380c0ce
Kiarash Kiani <kiani@kiarash.us>
2021-05-10 20:59:24 +0430

    init commit

--- aws_mlop_app/aws_mlop_app_stack.py
+++ aws_mlop_app/aws_mlop_app_stack.py
@@ -1,10 +1,5 @@
 from aws_cdk import core as cdk
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from model_deploy_construct import ModelDeploy
 
 
 class AwsMlopAppStack(cdk.Stack):
@@ -12,4 +7,9 @@ class AwsMlopAppStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        deploy = ModelDeploy(
+            self,
+            id=construct_id,
+            image_uri='939595455984.dkr.ecr.eu-west-1.amazonaws.com/dgl-citation-network:custom-torch-1.8',
+            execution_role="arn:aws:iam::939595455984:role/kiarash-sagemaker",
+        )
---
+++ aws_mlop_app/model_deploy_construct.py
@@ -0,0 +1,69 @@
+from attr import s
+from aws_cdk import core as cdk
+from aws_cdk import aws_stepfunctions as stepfunctions
+from aws_cdk import aws_stepfunctions_tasks as stepfunction_tasks
+from aws_cdk import aws_s3 as s3
+from aws_cdk import aws_ec2 as ec2
+
+
+class ModelDeploy(cdk.Construct):
+    def __init__(self, scope: cdk.Construct, id: str, *, image_uri: str, execution_role: str, instance_type: str='ml.m5.large') -> None:
+        super().__init__(scope, id)
+        
+        # creating bucket to save model artifacts in it.
+        artifact_bucket = s3.Bucket(
+            self,
+            id=f"{id}-bucket",
+            bucket_name=f"{id.lower()}-bucket"
+        )
+
+        # creating training job process with sagemaker
+        training_algorithm = stepfunction_tasks.AlgorithmSpecification(
+            training_image=stepfunction_tasks.DockerImage.from_registry(image_uri)
+        )
+
+        training_outputconfig = stepfunction_tasks.OutputDataConfig(
+            s3_output_location=stepfunction_tasks.S3Location.from_bucket(artifact_bucket, key_prefix='output')
+        )
+
+        training_resource = stepfunction_tasks.ResourceConfig(
+            instance_count=1,
+            instance_type=ec2.InstanceType(instance_type),
+            volume_size=cdk.Size.gibibytes(10)
+        )
+
+        training_dataset_channel = stepfunction_tasks.Channel(
+            channel_name="training-data",
+            data_source=stepfunction_tasks.DataSource(
+                s3_data_source=stepfunction_tasks.S3DataSource(
+                    s3_location=stepfunction_tasks.S3Location.from_bucket(artifact_bucket, key_prefix="output")
+                )
+            )
+        )
+
+        training_job = stepfunction_tasks.SageMakerCreateTrainingJob(
+            self,
+            id=f"{id}-training-job",
+            training_job_name=f"{id}TrainingJob",
+            # role=execution_role,
+            algorithm_specification=training_algorithm,
+            resource_config=training_resource,
+            output_data_config=training_outputconfig,
+            input_data_config=[training_dataset_channel],
+            hyperparameters={
+                'epochs': "100",
+                'learning-rate': '0.01'
+            },
+            tags={
+                "owner": "kiarash",
+                "project": "cdk-test"
+            }
+        )
+
+        state_machine = stepfunctions.StateMachine(
+            self, 
+            id="state-machine",
+            definition=training_job
+        )
+
+
